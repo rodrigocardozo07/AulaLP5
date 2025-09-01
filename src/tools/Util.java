7bb85fb45a388cc9717c0a1a80a4bf8a7e4d166f
rodrigocardozo07 <rodrigo.cardozo@estudante.ifms.edu.br>
2025-09-01 07:46:09 -0400

    Aula que criamos o habilitar.

---
+++ src/tools/Util.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+import javax.swing.JComponent;
+
+
+/**
+ *
+ * @author u07855463160
+ */
+public class Util {
+    public static void habilitar(boolean valor, JComponent ...componentes){
+        for (int i = 0; i < componentes.length; i++) {
+            componentes[i].setEnabled(valor);
+            
+        }
+    }
+    
+    
+}
